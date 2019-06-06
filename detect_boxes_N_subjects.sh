958e61273096db4f0e3b52121ce16cbeca12248c
Arjun Chandrasekaran <arjun9@gatech.edu>
2019-06-06 15:42:16 -0400

    Bash script to sequentially run YOLO on frames from each subject (and save them in separate prediction files)

---
+++ detect_boxes_N_subjects.sh
@@ -0,0 +1,12 @@
+# Run YOLO to detect objects, localize them for each subject  
+./darknet yolo getboxes cfg/yolo.cfg ./data/test_input__20170322_18742 results/test_preds__20170322_18742.txt yolo_15_toddlers.weights
+./darknet yolo getboxes cfg/yolo.cfg ./data/test_input__20161026_18625 results/test_preds__20161026_18625.txt yolo_15_toddlers.weights
+./darknet yolo getboxes cfg/yolo.cfg ./data/test_input__20170422_17919 results/test_preds__20170422_17919.txt yolo_15_toddlers.weights
+./darknet yolo getboxes cfg/yolo.cfg ./data/test_input__20170928_19615 results/test_preds__20170928_19615.txt yolo_15_toddlers.weights
+./darknet yolo getboxes cfg/yolo.cfg ./data/test_input__20170929_19954 results/test_preds__20170929_19954.txt yolo_15_toddlers.weights
+./darknet yolo getboxes cfg/yolo.cfg ./data/test_input__20171003_18996 results/test_preds__20171003_18996.txt yolo_15_toddlers.weights
+./darknet yolo getboxes cfg/yolo.cfg ./data/test_input__20171010_19544 results/test_preds__20171010_19544.txt yolo_15_toddlers.weights
+./darknet yolo getboxes cfg/yolo.cfg ./data/test_input__20171012_19694 results/test_preds__20171012_19694.txt yolo_15_toddlers.weights
+./darknet yolo getboxes cfg/yolo.cfg ./data/test_input__20171012_21015 results/test_preds__20171012_21015.txt yolo_15_toddlers.weights
+./darknet yolo getboxes cfg/yolo.cfg ./data/test_input__20180126_19859 results/test_preds__20180126_19859.txt yolo_15_toddlers.weights
+./darknet yolo getboxes cfg/yolo.cfg ./data/test_input__20180305_20510 results/test_preds__20180305_20510.txt yolo_15_toddlers.weights
